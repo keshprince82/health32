07970e007d6fac73c60114d8760dd3d3cda7466e
prince-neenopal <108261752+prince-neenopal@users.noreply.github.com>
2025-02-24 13:14:47 +0530

    loader

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -5,25 +5,31 @@ import { useState } from "react";
 
 import { Button, Col, Form, Input, Row } from "antd";
 import ModalPopUp from "../helperComponents/Modal";
+import Loader from "../helperComponents/loader";
 
 const Users = () => {
   const [userData, setUserData] = useState();
   const [userInAction, setUserInAction] = useState({});
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleEdit = (user) => {
     setUserInAction(user);
     setShowModal(true);
   };
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+  const fetchUsers = () => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
-        // fetch(`https://avatars.dicebear.com/v2/avataaars/${users[0].username}.svg?options[mood][]=happy`)
         setUserData(users);
+        setLoading(false);
       });
-  }, []);
+    }, 1000);
+
+  };
+
   const onFinish = (values) => {
     setUserData(
       userData?.map((item) => {
@@ -36,109 +42,133 @@ const Users = () => {
     setShowModal(false);
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  const handleFavourite = (user) =>{
-    if('favourite' in user){
-
-        user ={ ... user, favourite:!user.favourite}
-    }else{
-        user ={...user,favourite:true}
+  const handleFavourite = (user) => {
+    if ("favourite" in user) {
+      user = { ...user, favourite: !user.favourite };
+    } else {
+      user = { ...user, favourite: true };
     }
 
-  setUserData(  userData?.map((item) => {
+    setUserData(
+      userData?.map((item) => {
         if (item.id == user?.id) {
           item = { ...user };
         }
         return item;
-      }))
+      })
+    );
+  };
 
-  }
+  const handleDelete = (user) => {
+    setUserData(userData?.filter((item) => item.id != user.id));
+  };
 
-  const handleDelete = (user) =>{
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-  setUserData(  userData?.filter(item=>item.id != user.id))
-
-  }
   return (
     <>
-      <Row gutter={[10, 10]}>
-        {userData?.map((user) => {
-          return (
-            <Col xl={6} lg={8} md={12} xs={24}>
-              <UserCard user={user} handleEdit={handleEdit} handleFavourite={handleFavourite} handleDelete={handleDelete} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Row gutter={[10, 10]}>
+            {userData?.map((user) => {
+              return (
+                <Col xl={6} lg={8} md={12} xs={24}>
+                  <UserCard
+                    user={user}
+                    handleEdit={handleEdit}
+                    handleFavourite={handleFavourite}
+                    handleDelete={handleDelete}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
 
-      <ModalPopUp
-        open={showModal}
-        handleCancel={()=>{setShowModal(false)}}
-        
-        title="Update user"
-        footer={[
-          <Button
-            onClick={() => {
+          <ModalPopUp
+            open={showModal}
+            handleCancel={() => {
               setShowModal(false);
             }}
+            title="Update user"
+            footer={[
+              <Button
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                Cancel
+              </Button>,
+              <Button
+                form="myForm"
+                key="submit"
+                htmlType="submit"
+                type="primary"
+              >
+                Ok
+              </Button>,
+            ]}
           >
-            Cancel
-          </Button>,
-          <Button form="myForm" key="submit" htmlType="submit" type="primary">
-            Ok
-          </Button>,
-        ]}
-      >
-        <Form
-          id="myForm"
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{
-            name: userInAction ? userInAction.name : "",
-            email: userInAction ? userInAction.email : "",
-            phone: userInAction ? userInAction.phone : "",
-            website: userInAction ? userInAction.website : "",
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: "Please input your name!" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ type: 'email',required: true, message:"Please input your email correctly" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Phone"
-            name="phone"
-            rules={[{ required: true, message: "Please input your phone!" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Website"
-            name="website"
-            rules={[{ required: true, message: "Please input your website!" }]}
-          >
-            <Input />
-          </Form.Item>
-        </Form>
-      </ModalPopUp>
-
+            <Form
+              id="myForm"
+              name="basic"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{ maxWidth: 600 }}
+              initialValues={{
+                name: userInAction ? userInAction.name : "",
+                email: userInAction ? userInAction.email : "",
+                phone: userInAction ? userInAction.phone : "",
+                website: userInAction ? userInAction.website : "",
+              }}
+              onFinish={onFinish}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[{ required: true, message: "Please input your name!" }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    type: "email",
+                    required: true,
+                    message: "Please input your email correctly",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Phone"
+                name="phone"
+                rules={[
+                  { required: true, message: "Please input your phone!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Website"
+                name="website"
+                rules={[
+                  { required: true, message: "Please input your website!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Form>
+          </ModalPopUp>
+        </>
+      )}
       {/* <UserCard
   
   
